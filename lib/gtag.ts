421053d577fde505f813dfc25faaa5543cfdbbcb
abuayaan01 <abuayaan01@gmail.com>
2026-04-07 20:58:28 +0530

    Fix: Tag Manager Lable Added

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,7 +1,7 @@
 export const fireCallConversion = () => {
     if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
       window.gtag('event', 'conversion', {
-        send_to: 'AW-17927466366'
+        send_to: 'AW-17927466366/Wv64CJvL6PkbEP7aveRC'
       });
     }
   };
